6a94b6b007da66348b81fd92ef7542bfe9e2275e
haydeniza <izabellyhayden@hotmail.com>
2024-06-25 09:56:51 -0400

    alteração de hoje2

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $cliente = new Clienteclass($con);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Aplica</title>
+    <title>Minha2</title>
 
   <link rel="stylesheet" href="./assets/bootstrap-5.3.3-dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="./assets/css/meucss.css">
